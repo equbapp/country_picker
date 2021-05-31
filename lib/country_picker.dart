3d032e5db25257225d1f339ad96a9a874ee2fef3
Surafeljava <surafelk27@gmail.com>
2021-05-31 15:18:06 +0300

    feat: ready for release

--- lib/country_picker.dart
+++ lib/country_picker.dart
@@ -7,13 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CountryPicker {
-  static const MethodChannel _channel = const MethodChannel('country_picker');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   static Future showCountryPicker(
       {@required BuildContext context,
       @required String title,
@@ -24,7 +17,12 @@ class CountryPicker {
       double borderRadius = 10.0}) {
     assert(context != null, "context is null!!");
     assert(willDisplayWidget != null, "willDisplayWidget is null!!");
-    assert(title != null, "Title is null!!");
+    assert(title != null, "title is null!!");
+    assert(onSelected != null, "On Selected is null!!");
+    assert(backgroundColor != null, "backgroundColor is null!!");
+    assert(foregroundColor != null, "foregroundColor is null!!");
+    assert(borderRadius != null, "borderRadius is null!!");
+
     return showModalBottomSheet(
       context: context,
       isScrollControlled: true,
--- lib/picker_view.dart
+++ lib/picker_view.dart
@@ -35,118 +35,122 @@ class _PickerViewState extends State<PickerView> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        height: MediaQuery.of(context).size.height-150,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15.0)
-        ),
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Container(
-                  width: 100,
-                  height: 8,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4.0),
-                    color: Colors.grey[500],
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).viewInsets.bottom==0 ? MediaQuery.of(context).size.height/1.3 : MediaQuery.of(context).size.height*0.5,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15.0)),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    width: 100,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4.0),
+                      color: Colors.grey[500],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding:
-              const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      onChanged: (val) {
-                        setState(() {
-                          searchText = val;
-                        });
-                        searchThis(val);
-                      },
-                      autofocus: false,
-                      keyboardType: TextInputType.text,
-                      style: TextStyle(
-                        fontSize: 17.0,
-                        color: Colors.grey[800],
-                        letterSpacing: 0.5,
-                        fontWeight: FontWeight.w400,
-                      ),
-                      decoration: InputDecoration(
-                        hintText: 'Search',
-                        hintStyle: TextStyle(
+              Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        onChanged: (val) {
+                          setState(() {
+                            searchText = val;
+                          });
+                          searchThis(val);
+                        },
+                        autofocus: false,
+                        keyboardType: TextInputType.text,
+                        style: TextStyle(
                           fontSize: 17.0,
-                          color: Colors.grey[700],
-                          fontWeight: FontWeight.w300,
+                          color: Colors.grey[800],
+                          letterSpacing: 0.5,
+                          fontWeight: FontWeight.w400,
                         ),
-                        contentPadding:
-                        EdgeInsets.only(left: 20.0, right: 20.0, top: 10),
-                        filled: true,
-                        fillColor: Colors.grey[200],
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                          borderSide:
-                          BorderSide(color: Colors.transparent, width: 1.5),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                          borderSide:
-                          BorderSide(color: Colors.transparent, width: 1.5),
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          hintStyle: TextStyle(
+                            fontSize: 17.0,
+                            color: Colors.grey[700],
+                            fontWeight: FontWeight.w300,
+                          ),
+                          contentPadding:
+                          EdgeInsets.only(left: 20.0, right: 20.0, top: 10),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide:
+                            BorderSide(color: Colors.transparent, width: 1.5),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            borderSide:
+                            BorderSide(color: Colors.transparent, width: 1.5),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: (){
-                      //search
-                    },
-                  ),
-                ],
+                    IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: (){
+                        if(searchText!=''){
+                          searchThis(searchText);
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: allCodes.length==0 ? Container(width: 50, height: 50, child: CircularProgressIndicator(),) :
-              searchText=='' ? ListView.builder(
-                itemCount: allCodes.length,
-                itemBuilder: (context, index) {
-                  CountryCodeInfo code = allCodes[index];
-                  return ListTile(
-                    leading: Text(code.flag),
-                    title: Text(code.country),
-                    subtitle: Text(code.dialCode),
-                    onTap: (){
-                      print(code.dialCode);
-                      widget.onPicked(code);
-                      widget.onClose(widget.context);
-                    },
-                  );
-                },
-              ) : ListView.builder(
-                itemCount: searchedCodes.length,
-                itemBuilder: (context, index) {
-                  CountryCodeInfo code = searchedCodes[index];
-                  return ListTile(
-                    leading: Text(code.flag),
-                    title: Text(code.country),
-                    subtitle: Text(code.dialCode),
-                    onTap: (){
-                      print(code.dialCode);
-                      widget.onPicked(code);
-                      widget.onClose(widget.context);
-                    },
-                  );
-                },
+              Expanded(
+                child: allCodes.length==0 ? Container(width: 50, height: 50, child: CircularProgressIndicator(),) :
+                searchText=='' ? ListView.builder(
+                  itemCount: allCodes.length,
+                  itemBuilder: (context, index) {
+                    CountryCodeInfo code = allCodes[index];
+                    return ListTile(
+                      leading: Text(code.flag),
+                      title: Text(code.country),
+                      subtitle: Text(code.dialCode),
+                      onTap: (){
+                        print(code.dialCode);
+                        widget.onPicked(code);
+                        widget.onClose(widget.context);
+                      },
+                    );
+                  },
+                ) : ListView.builder(
+                  itemCount: searchedCodes.length,
+                  itemBuilder: (context, index) {
+                    CountryCodeInfo code = searchedCodes[index];
+                    return ListTile(
+                      leading: Text(code.flag),
+                      title: Text(code.country),
+                      subtitle: Text(code.dialCode),
+                      onTap: (){
+                        print(code.dialCode);
+                        widget.onPicked(code);
+                        widget.onClose(widget.context);
+                      },
+                    );
+                  },
 
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
